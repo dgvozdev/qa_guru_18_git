d3010c571f73a32cbcce1d89dc16f1c9d9f328f9
Denis Gvozdev <dngvozdev@yandex.com>
2025-11-30 18:43:34 +0300

    added getMesage() method

--- src/main/java/ru/wyrd/Main.java
+++ src/main/java/ru/wyrd/Main.java
@@ -2,6 +2,10 @@ package ru.wyrd;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru");
+        System.out.println(getMesage());
+    }
+
+    private static String getMesage() {
+        return "Hello qa.guru";
     }
 }
